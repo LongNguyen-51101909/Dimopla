9bce90897de9078da2c963cf2f0a700c4741b52a
ttvi-cse <ttvicse@gmail.com>
2015-12-24 14:26:50 +0700

    update search 'donhang'.

--- application/layouts/tim/donhang.php
+++ application/layouts/tim/donhang.php
@@ -7,12 +7,13 @@
     if($this->param->isPost())
     {
         $model = new Model_Donhang();
+        $kh = new Model_Khachhang();
         $key = $this->param->getParam("key");
         $dataSearch = $model->searchByKey($key);
         
         if($dataSearch != false){
     
-            $title = array("Mã Đơn Hàng","Tên Đơn Hàng","Ngày Đặt","Số Mét Vải","Tùy Chỉnh");
+            $title = array("Mã Đơn Hàng","Tên Đơn Hàng","Ngày Đặt","Tiền Đặt Hàng","Khách Hàng","Số Mét Vải","Tùy Chỉnh");
             
             $maincontent = array();
             foreach ($dataSearch as $item)
@@ -21,8 +22,11 @@
                 $item['MaDonHang'],
                 $item['TenDonHang'],
                 $item['NgayDat'],
+                $item['TienDatHang'],
+                $kh->getWhere($item['MaKhachHang'])[0]['TenKhachHang'],
                 $item['SoMetVai'],
-                
+                '<a href="'.HOST_PROJECT."/index/chinhsua/donhang/true/makhachhang/".$item['MaKhachHang']."/madonhang/".$item['MaDonHang'].'/option/donhang">Sửa</a>&nbsp|&nbsp'.
+                    '<a href="'.HOST_PROJECT."/index/xoa/donhang/true/makhachhang/".$item['MaKhachHang']."/madonhang/".$item["MaDonHang"].'/option/xem/" onclick="return confirm('."'bạn có chắc muốn xóa ?'".')">Xóa</a>',
             );
             $maincontent[] = $content;
     }
