be170f2027a61e6d451bd6af72dc0d2a491a6347
LongNguyen-51101909 <longnguyenqu@gmail.com>
2015-12-30 09:19:00 +0700

    Hint value Key when user input key Searchs/khachhang.php

--- application/forms/Searchs/khachhang.php
+++ application/forms/Searchs/khachhang.php
@@ -15,7 +15,7 @@ class Form_Searchs_Khachhang extends Zend_Form {
         //$chonkhohang->setAttrib('class', 'formEdit')->setValue($khohang[0]['MaKho']);
 
 //         $khohang= $this->createElement('select', 'khachhang', array('multioptions'=>$opKhoHang,'decorators' => array('ViewHelper'),));
-        $khachhangtim= $this->createElement('text', 'khachhangtim', array('decorators' => array('ViewHelper'),));
+        $khachhangtim= $this->createElement('text', 'khachhangtim', array('decorators' => array('ViewHelper'),'value' => $_POST["khachhangtim"]));
         $them =  $this->createElement('submit', 'them', array('decorators' => array('ViewHelper'),'label'=>'Chọn'));
 
         $khachhangtim->setAttrib('class', 'formEdit');
